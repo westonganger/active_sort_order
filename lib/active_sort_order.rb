e3c8191f9cb15b93c6114d0e3675a5d93ef958f4
Weston Ganger <weston@westonganger.com>
2023-02-13 21:46:21 -0800

    Utilize active record lazy loading (#1)

--- lib/active_sort_order.rb
+++ lib/active_sort_order.rb
@@ -1,11 +1,15 @@
-require "active_record"
+require "active_sort_order/version"
 
-require "active_sort_order/concerns/sort_order_concern"
+require "active_support/lazy_load_hooks"
 
-module ActiveSortOrder 
-  extend ActiveSupport::Concern
+ActiveSupport.on_load(:active_record) do
+  require "active_sort_order/concerns/sort_order_concern"
 
-  included do
-    include ActiveSortOrder::SortOrderConcern
+  module ActiveSortOrder
+    extend ActiveSupport::Concern
+
+    included do
+      include ActiveSortOrder::SortOrderConcern
+    end
   end
 end
